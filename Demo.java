11e024f0ae659975df2adb88c8da92419215c8ea
seemapoudel58 <simapoudel6@gmail.com>
2024-08-07 07:32:24 +0545

    program

--- Demo.java
+++ Demo.java
@@ -1,7 +1,9 @@
 class Demo {
     public static void main(String []args){
+        int s=0;
         for(int i=0; i<args.length; i++){
-        System.out.println(args[i]);
+            s=s+Integer.parseInt(args[i]);
+            System.out.println(s);
         }
     }
 }
